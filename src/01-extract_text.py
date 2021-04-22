405339411d1a15e10fba60de4c3ab50d1189ad0c
Icemole <nahurobe3@gmail.com>
2021-04-22 12:51:22 +0200

    Improved XML to plain text extraction
    The script now accepts an 'extract_from' option which allows it to set the directory where to look for the en/ and es/ files

--- src/01-extract_text.py
+++ src/01-extract_text.py
@@ -8,26 +8,28 @@ def get_text_from_xml_line(line):
     return None if result is None else result.group(1)
 
 
-## Extracts all text from all .xml files of a single language
-## passed as a parameter, given the format of the PAN2021 competition.
-def extract_lang(lang, filepath, group_writer):
+# Extracts all text from all .xml files of a single language directory
+# passed as a parameter, given the format of the PAN2021 competition.
+def extract_lang(fr, to, group_writer):
+    lang = fr.rstrip("/").split("/")[-1]
+    print(lang)
     if group_writer:
-        nonhaters_file = filepath + "nonhaters_" + lang + "_grouped.txt"
-        haters_file = filepath + "haters_" + lang + "_grouped.txt"
+        nonhaters_file = to + "nonhaters_" + lang + "_grouped.txt"
+        haters_file = to + "haters_" + lang + "_grouped.txt"
     else:
-        nonhaters_file = filepath + "nonhaters_" + lang + ".txt"
-        haters_file = filepath + "haters_" + lang + ".txt"
+        nonhaters_file = to + "nonhaters_" + lang + ".txt"
+        haters_file = to + "haters_" + lang + ".txt"
     err_file = "data/err.txt"
 
     with open(nonhaters_file, "w") as f_nonhaters, \
             open(haters_file, "w") as f_haters, \
             open(err_file, "w") as f_err:
-        for xml in os.listdir(filepath):
-            #print(filepath + xml)
+        for xml in os.listdir(fr):
+            #print(fr + xml)
             if xml == "truth.txt":
                 # Not interested in this file (contains references)
                 continue
-            with open(filepath + xml, "r") as f:
+            with open(fr + xml, "r") as f:
                 f_contents = list(f)
                 author_class = f_contents[0].split()[2].split('"')[1]
                 if author_class == "0":
@@ -62,13 +64,15 @@ def main():
             raise argparse.ArgumentTypeError('Boolean value expected.')
 
     parser = argparse.ArgumentParser()
+    parser.add_argument('--extract_from', default='data/original_dataset',
+                        help='Where to look for the en/ and es/ directories')
     parser.add_argument('--extract_to', default='data/plain_text/',
                         help='Where to write the output of the data extraction')
     parser.add_argument('--group_writer', type=str2bool, nargs='?', const=True, default=False,
                         help='If set to True, one line of the extracted text will correspond to all sentences of a single writer')
     args = parser.parse_args()
-    extract_lang("en", args.extract_to, args.group_writer)
-    extract_lang("es", args.extract_to, args.group_writer)
+    extract_lang(args.extract_from + "/en/", args.extract_to + "/", args.group_writer)
+    extract_lang(args.extract_from + "/es/", args.extract_to + "/", args.group_writer)
 
 
 if __name__ == "__main__":
